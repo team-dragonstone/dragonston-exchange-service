a2d4c9b0698b9f32ef790fca93f3006588588690
xinxiaoai.liu <xinxiaoai.liu@xinxiaoais-air.fios-router.home>
2019-05-28 10:54:55 -0400

    1st commit

---
+++ src/main/java/com/training/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,23 @@
+package com.training.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private ExchangeValueRepository repo;
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		ExchangeValue ev= repo.findByFromAndTo(from, to);
+		ev.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return ev;
+	}
+
+}
---
+++ src/main/java/com/training/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,60 @@
+package com.training.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class ExchangeValue {
+	@Id
+	private Long id;
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	@Column(name="conversion_rate")
+	private BigDecimal conversionRate;
+	private int port;
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getConversionRate() {
+		return conversionRate;
+	}
+	public void setConversionRate(BigDecimal conversionrate) {
+		this.conversionRate = conversionrate;
+	}
+	public ExchangeValue( Long id, String from, String to, BigDecimal conversionrate) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.id = id;
+		this.conversionRate = conversionrate;
+	}
+	
+	protected ExchangeValue () {
+		
+	}
+}
