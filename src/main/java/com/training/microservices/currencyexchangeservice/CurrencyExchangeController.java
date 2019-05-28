4a9808db170c8fb03627311bf2fd43ec4e86fa3f
keyundick <keyundick@keyuns-mbp.fios-router.home>
2019-05-28 15:39:33 -0400

    Updated CurrencyExchangeController with find all, and addExchange

--- src/main/java/com/training/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/training/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,9 +1,13 @@
 package com.training.microservices.currencyexchangeservice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +22,25 @@ public class CurrencyExchangeController {
 		ExchangeValue ev= repo.findByFromAndTo(from, to);
 		ev.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		return ev;
+	}
+	
+	@GetMapping("/currency-exchange/allExchangeValues")
+	public List<ExchangeValue> retrieveAllExchangeValues() {
+		
+		return repo.findAll();
+	}
+	
+	@PostMapping("/currency-exchange/addExchange")
+	public void postExchangeValue(@RequestBody ExchangeValue exchangeValue) throws Exception {
+		
+		if (!repo.findById(exchangeValue.getId()).isEmpty()
+				|| repo.findByFromAndTo(exchangeValue.getFrom(), exchangeValue.getTo()) != null) {
+			//TODO: CIC guys might have an error page or something we can direct this message to.
+			throw new Exception("An exchangeValue with id: " + exchangeValue.getId() 
+				+ " or from: " + exchangeValue.getFrom() + " and to: " + exchangeValue.getTo() + " already exists");
+		}
+		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		repo.save(exchangeValue);
 	}
 
 }
